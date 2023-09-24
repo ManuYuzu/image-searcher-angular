fd90213b25d62088333fb18685d885662d0a9003
ManuYuzu <manuaglez@gmail.com>
2023-09-24 23:25:31 +0100

    chore: environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
+/**
+ * This should never be uploaded to any service.
+ */
+
 export const environment = {
   pixabayURL: 'https://pixabay.com/api/',
   pixabayAPIKey: '34573560-b4b29946062bf53b893e1ec40'
