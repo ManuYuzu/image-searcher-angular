5ab380b6c3fa234e8aa5db22b62d61f1278337e0
ManuYuzu <manuaglez@gmail.com>
2023-09-27 10:50:46 +0100

    Create interfaces from API response

---
+++ src/app/interfaces/APIInterfaces.ts
@@ -0,0 +1,30 @@
+export interface APIResults {
+collections: number;
+comments: number;
+downloads: number;
+id: number;
+imageHeight: number;
+imageSize: number;
+imageWidth: number;
+largeImageURL: string;
+likes: number;
+pageURL: string;
+previewHeight: number;
+previewURL: string;
+previewWidth: number;
+tags: string;
+type: string;
+user: string;
+userImageURL: string;
+user_id: number;
+views: number;
+webformatHeight: number;
+webformatURL: string;
+webformatWidth: number;
+}
+
+export interface APIResponse {
+  hits: APIResults[];
+  total: number;
+  totalHits: number
+}
